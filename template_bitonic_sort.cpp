7aad46d514f035b92df1eb61ce87570e34ff931f
xxRanger <anxin@anxins-MacBook-Pro.local>
2020-04-11 01:03:49 +0800

    template bitonic sort

--- template_bitonic_sort.cpp
+++ template_bitonic_sort.cpp
@@ -4,9 +4,6 @@
 using namespace std;
 
 template<typename T>
-void __bitonic_sort(vector<T> &arr, int start_index, int end_index, int sort_direction);
-
-template<typename T>
 void compare_and_swap(T &a, T &b, int sort_direction) {
     if (sort_direction == 0) {
         if (a > b) std::swap(a, b);
@@ -15,47 +12,46 @@ void compare_and_swap(T &a, T &b, int sort_direction) {
     }
 }
 
-
 template<typename T>
-void generate_bitonic_sequence(vector<T> &arr, int start_index, int end_index, int sort_direction) {
-    int level = log2(end_index - start_index);  //
-    for (int i = 1; i < level; i++) {
-        int gap = pow(2, i);
-        for (int j = start_index; j < end_index; j += gap) {
-            __bitonic_sort(arr, j, j + gap, ((j-start_index)/gap % 2) ^ sort_direction);
+void __bitonic_sort(vector<T> &arr, int start_index, int end_index, int sort_direction) {
+    //up to bottom
+    // Every time use __bitonic_sort, assume the sequence is already bitonic;
+    // an recursive version, recursively swap upper part and bottom party, 
+//    if (start_index + 1 >= end_index) return;
+//    int mid = (start_index + end_index) / 2;
+//    for (int i = start_index; i < mid; i++) {
+//        compare_and_swap(arr[i], arr[mid + i - start_index], sort_direction);
+//    }
+//    __bitonic_sort(arr, start_index, mid, sort_direction);
+//    __bitonic_sort(arr, mid, end_index, sort_direction);
+    
+//  an iterative version
+    int ele = end_index - start_index;
+    for(int j= ele; j>=2; j/=2) {
+        for(int k=start_index;k<end_index;k+=j) {
+            for(int i=k;i<k+j/2;i++) {
+                compare_and_swap(arr[i], arr[j/2 + i], sort_direction);
+            }
         }
     }
 }
 
 template<typename T>
-void bitonic_swap(vector<T> &arr, int start_index, int end_index, int sort_direction) {
-    if (start_index + 1 >= end_index) return;
-    int mid = (start_index + end_index) / 2;
-    for (int i = start_index; i < mid; i++) {
-        compare_and_swap(arr[i], arr[mid + i - start_index], sort_direction);
-    }
-    bitonic_swap(arr, start_index, mid, sort_direction);
-    bitonic_swap(arr, mid, end_index, sort_direction);
-}
-
-template<typename T>
-void __bitonic_sort(vector<T> &arr, int start_index, int end_index, int sort_direction) {
-    generate_bitonic_sequence(arr, start_index, end_index, sort_direction);
-    bitonic_swap(arr, start_index, end_index, sort_direction);
-}
-
-template<typename T>
 void bitonic_sort(vector<T> &arr, int sort_direction) {
-    __bitonic_sort(arr, 0, arr.size(), sort_direction);
+    // bottom to up
+    // Every time use __bitonic_sort, assume the sequence is already bitonic;
+    for (int j = 2; j <= arr.size(); j*=2) {
+        for (int i = 0; i < arr.size(); i += j) {         // can use openmp accelerate this part
+            __bitonic_sort(arr, i, i + j, (i/j % 2) ^ sort_direction);
+        }
+    }
 }
 
-void test_bitonic_sort_template() {
-    vector<string> arr(1024);
+void test_bitonic_sort_template(int N) {
+    vector<string> arr(N);
     for(int i=0;i<arr.size();i++) {
         arr[i] = to_string(rand()%1000);
-        cout<<arr[i]<<" ";
     }
-    cout<<endl;
 
     bitonic_sort(arr, 1);
     for(int i=0;i<arr.size();i++) {
@@ -65,6 +61,6 @@ void test_bitonic_sort_template() {
 }
 
 int main() {
-    test_bitonic_sort_template();
+    test_bitonic_sort_template(1024);
     return 1;
 }
